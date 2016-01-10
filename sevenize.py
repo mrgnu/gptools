bb7746e9d839b7905fe50250faf6feb390bc62d7
mrgnu <mr.gnu.jr@gmail.com>
2016-01-10 16:02:29 +0100

    re-tune 7-string as well as 6-string tracks

--- sevenize.py
+++ sevenize.py
@@ -2,7 +2,8 @@
 import sys
 
 if len(sys.argv) != 3:
-    print("This script will add a D5 string as new first string for all 6-string tracks.")
+    print("This script will add a D5 string as new first string for all " \
+          "6-string tracks, and re-tune 3rd string from E4 to F4 as needed.")
     print("Usage:", sys.argv[0], "<infile> <outfile>", file=sys.stderr)
     sys.exit(1)
 
@@ -17,47 +18,44 @@ song = guitarpro.parse(src)
 print("Processing", song.title)
 
 # iterate over tracks, adding D5 string as new first string for all
-# 6-string tracks
+# 6-string tracks, and re-tuning 3rd string from E4 to F4 when needed
 for track in song.tracks:
-    if len(track.strings) != 6 or track.isPercussionTrack:
-        continue
+    if track.isPercussionTrack: continue
 
-    print("* Adding D5 string to", track.name)
+    if len(track.strings) == 6:
+        print("* Adding D5 string to", track.name)
 
-    # create D5 string
-    d5 = guitarpro.base.GuitarString(1, 62)
-    # modify index of existing string
-    for string in track.strings:
-        string.number = string.number + 1
-    # add
-    track.strings.insert(0, d5)
+        # create D5 string
+        d5 = guitarpro.base.GuitarString(1, 62)
+        # modify index of existing string
+        for string in track.strings:
+            string.number = string.number + 1
+        # add
+        track.strings.insert(0, d5)
 
-    # move all notes down one string
-    for measure in track.measures:
-        for voice in measure.voices:
-            for beat in voice.beats:
-                for note in beat.notes:
-                    note.string = note.string + 1
+        # move all notes down one string
+        for measure in track.measures:
+            for voice in measure.voices:
+                for beat in voice.beats:
+                    for note in beat.notes:
+                        note.string = note.string + 1
 
     # re-tune 3rd string from E4 to F4 if needed
-    if track.strings[2].value != 52:
-        print("  WARNING: not retuning string 3, tuned to",
-              str(track.strings[2].value))
-        continue
+    if track.strings[2].value == 52:
+        print("* Retuning string 3 of track", track.name)
+        track.strings[2].value = track.strings[2].value + 1
 
-    track.strings[2].value = track.strings[2].value + 1
-
-    for measure in track.measures:
-        for voice in measure.voices:
-            for beat in voice.beats:
-                for note in beat.notes:
-                    if note.string != 3:
-                        continue
-                    if note.value == 0:
-                        print("  WARNING: failed to re-tune note in measure", measure.number)
-                        note.value = 35
-                    else:
-                        note.value = note.value - 1
+        for measure in track.measures:
+            for voice in measure.voices:
+                for beat in voice.beats:
+                    for note in beat.notes:
+                        if note.string != 3:
+                            continue
+                        if note.value == 0:
+                            print("  WARNING: failed to re-tune note in measure", measure.number)
+                            note.value = 35
+                        else:
+                            note.value = note.value - 1
 
 print("Writing to", dst)
 guitarpro.write(song, dst)
